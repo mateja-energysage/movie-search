ee0870256f847a181abaa5ffd06364605a08e33a
Mateja Cosovic <mateja.cosovic@energysage.com>
2024-03-24 12:40:59 +0100

    fix: dependency versions, Lambda config

--- movie_search/movie_search_stack.py
+++ movie_search/movie_search_stack.py
@@ -1,5 +1,5 @@
 from constructs import Construct
-from aws_cdk import Stack, RemovalPolicy
+from aws_cdk import Stack, RemovalPolicy, Duration
 from aws_cdk.aws_lambda import Runtime, Function, Code, Architecture
 import aws_cdk.aws_apigateway as apigateway
 from aws_cdk.aws_dynamodb import (
@@ -31,6 +31,8 @@ class MovieSearchStack(Stack):
             code=Code.from_asset("artifact.zip"),
             handler="api.main.handler",
             environment={"USER_TABLE_NAME": self.user_table.table_name},
+            timeout=Duration.seconds(10),
+            memory_size=256,
         )
         self.user_table.grant_read_write_data(self.movie_handler_lambda)
 
