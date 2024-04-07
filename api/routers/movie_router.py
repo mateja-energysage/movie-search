61b746a90d5c0068ac47b44a44a94b7a832b523d
Mateja Cosovic <mateja.cosovic@energysage.com>
2024-04-07 20:03:24 +0200

    feat: Implements initial search, bugfixes

--- api/routers/movie_router.py
+++ api/routers/movie_router.py
@@ -1,12 +1,12 @@
 from typing import List, Any, Dict
 from fastapi import APIRouter, Depends
 from api.auth import verify_token
-from api.models.movie_dtos import MovieDTO, SearchBodyDTO, NewMovieDTO
+from api.models.movie_dtos import MovieDTO, SearchBodyDTO, NewMovieDTO, SearchResultDTO
 from api.utilities.opensearch_operations import (
     index_document,
     delete_index,
     get_movies_from_os,
-    get_movie_by_id_os,
+    get_movie_by_id_os, set_result_window,
 )
 from api.utilities.s3_operations import process_csv_file
 
@@ -33,19 +33,22 @@ def add_movie(
 )
 def add_movies(chunks: int | None = 10) -> dict[str, str]:
     process_csv_file(chunks)
+    set_result_window()
     return {"status": "success"}
 
 
-@router.get(
-    path="/movies",
-    response_model=List[MovieDTO],
+@router.post(
+    path="/movies/search",
+    response_model=SearchResultDTO,
     description="Get a list of movies matching params.",
     dependencies=[Depends(verify_token)],
 )
 def get_movies(
-    page: int | None = 1, search_body: SearchBodyDTO | None = None
-) -> List[MovieDTO]:
-    return get_movies_from_os(page=page, params=search_body)
+    search_body: SearchBodyDTO,
+    page: int | None = 1,
+    sort_by: str | None = "vote_average",
+) -> SearchResultDTO:
+    return get_movies_from_os(page=page, sort_by=sort_by, params=search_body)
 
 
 @router.get(
